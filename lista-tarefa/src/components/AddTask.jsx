438ac63e821c4fb0e865d23ebffc86eeceb5221d
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-09-23 14:11:51 -0300

    setInputData

--- lista-tarefa/src/components/AddTask.jsx
+++ lista-tarefa/src/components/AddTask.jsx
@@ -3,10 +3,17 @@ import './AddTask.css';
 import Button from './Button';
 
 const AddTask = () => {
+    const [inputData, setInputData] = useState('');
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+        
+    }
+
     return ( 
 
     <div className='add-task-container'>
-        <input className='add-task-input' type="text" />
+        <input onChange={handleInputChange} value={inputData} className='add-task-input' type="text" />
         <div className='add-task-button-container'>
             <Button>Adicionar</Button>
         </div>
