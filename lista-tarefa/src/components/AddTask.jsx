db3505a8f30ab4f243bd80e16fa4affffc67bee9
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-09-23 12:42:13 -0300

    input + css

--- lista-tarefa/src/components/AddTask.jsx
+++ lista-tarefa/src/components/AddTask.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './AddTask.css';
 
 const AddTask = () => {
     return ( 
-        <input type="text" />
-     );
+
+    <div className='add-task-container'>
+        <input className='add-task-input' type="text" />
+
+    </div>
+    );
 }
  
 export default AddTask;
